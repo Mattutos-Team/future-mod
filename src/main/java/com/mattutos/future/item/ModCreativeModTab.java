19b30ad3d0deb1514baf33eb0c28faa72743321a
relrypesan <relrypesan@gmail.com>
2024-04-04 21:25:27 -0300

    Ajustando para biblioteca da comunidade e otimização de codigo

--- src/main/java/com/mattutos/future/item/ModCreativeModTab.java
+++ src/main/java/com/mattutos/future/item/ModCreativeModTab.java
@@ -10,26 +10,29 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModCreativeModTab {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FutureMod.MOD_ID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FutureMod.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> FUTURE_MOD_TAB = CREATIVE_MODE_TAB.register("future_mod_tab", () -> CreativeModeTab.builder()
-            .icon(() -> new ItemStack(ModItems.WOOD_CIRCUIT.get())).title(Component.translatable("creative.future_mod_tab"))
-            .displayItems(((itemDisplayParameters, output) -> {
+    public static final RegistryObject<CreativeModeTab> FUTURE_MOD_TAB =
+            CREATIVE_MODE_TAB.register("future_mod_tab", () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ModItems.WOOD_CIRCUIT.get()))
+                    .title(Component.translatable("creative.future_mod_tab"))
+                    .displayItems(((itemDisplayParameters, output) -> {
 
-                //ITEMS
-                output.accept(ModItems.WOOD_CIRCUIT.get());
-                output.accept(ModItems.COOPER_CIRCUIT.get());
-                output.accept(ModItems.REDSTONE_CHIP.get());
-                output.accept(ModItems.VARNISH.get());
-                output.accept(ModItems.VARNISHED_WOOD.get());
+                        //ITEMS
+                        output.accept(ModItems.WOOD_CIRCUIT.get());
+                        output.accept(ModItems.COOPER_CIRCUIT.get());
+                        output.accept(ModItems.REDSTONE_CHIP.get());
+                        output.accept(ModItems.VARNISH.get());
+                        output.accept(ModItems.VARNISHED_WOOD.get());
 
-                //FUEL
+                        //FUEL
 
-                //FOODS
+                        //FOODS
 
-                //BLOCKS
-            }))
-            .build());
+                        //BLOCKS
+                    }))
+                    .build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TAB.register(eventBus);
