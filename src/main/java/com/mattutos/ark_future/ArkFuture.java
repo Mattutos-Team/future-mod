b54cabbcedf10d996e049b6972409703f213f0dc
PeterBarboza <pedro.barboza.dev@gmail.com>
2025-03-24 09:59:55 -0300

    feat: Adding "Ancient ore" basic items but without any functionality, just items itself (textures are not being loaded)

--- src/main/java/com/mattutos/ark_future/ArkFuture.java
+++ src/main/java/com/mattutos/ark_future/ArkFuture.java
@@ -15,7 +15,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import org.slf4j.Logger;
@@ -39,12 +38,16 @@ public class ArkFuture {
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
 
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+//        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            // TODO: ...
+            event.accept(ItemInit.ANCIENT_ORE_ITEM);
+            event.accept(ItemInit.ANCIENT_ORE_BLOCK_ITEM);
+            event.accept(ItemInit.ANCIENT_ORE_INGOT_ITEM);
+            event.accept(ItemInit.ANCIENT_ORE_VEIN_BLOCK_ITEM);
+            event.accept(ItemInit.DEEPSLATE_ANCIENT_ORE_VEIN_BLOCK_ITEM);
         }
     }
 }
--- src/main/java/com/mattutos/ark_future/init/ItemInit.java
+++ src/main/java/com/mattutos/ark_future/init/ItemInit.java
@@ -1,13 +1,83 @@
 package com.mattutos.ark_future.init;
 
 import com.mattutos.ark_future.ArkFuture;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ArkFuture.MOD_ID);
+
+    public static final RegistryObject<Item> ANCIENT_ORE_ITEM = ITEMS.register(
+            "ancient_ore",
+            () -> new Item(
+                    new Item.Properties().useItemDescriptionPrefix().setId(
+                            ResourceKey.create(
+                                    Registries.ITEM,
+                                    ResourceLocation.parse(
+                                            String.format("%s:%s", ArkFuture.MOD_ID, "ancient_ore")
+                                    )
+                            )
+                    )
+            )
+    );
+
+    public static final RegistryObject<Item> ANCIENT_ORE_BLOCK_ITEM = ITEMS.register(
+            "ancient_ore_block",
+            () -> new Item(
+                    new Item.Properties().useItemDescriptionPrefix().setId(
+                            ResourceKey.create(
+                                    Registries.ITEM, ResourceLocation.parse(
+                                            String.format("%s:%s", ArkFuture.MOD_ID, "ancient_ore_block")
+                                    )
+                            )
+                    )
+            )
+    );
+
+    public static final RegistryObject<Item> ANCIENT_ORE_INGOT_ITEM = ITEMS.register(
+            "ancient_ore_ingot",
+        () -> new Item(
+                new Item.Properties().useItemDescriptionPrefix().setId(
+                        ResourceKey.create(
+                                Registries.ITEM, ResourceLocation.parse(
+                                        String.format("%s:%s", ArkFuture.MOD_ID, "ancient_ore_ingot")
+                                )
+                        )
+                )
+        )
+    );
+
+    public static final RegistryObject<Item> ANCIENT_ORE_VEIN_BLOCK_ITEM = ITEMS.register(
+            "ancient_ore_vein_block",
+            () -> new Item(
+                    new Item.Properties().useItemDescriptionPrefix().setId(
+                            ResourceKey.create(
+                                    Registries.ITEM, ResourceLocation.parse(
+                                            String.format("%s:%s", ArkFuture.MOD_ID, "ancient_ore_vein_block")
+                                    )
+                            )
+                    )
+            )
+    );
+
+    public static final RegistryObject<Item> DEEPSLATE_ANCIENT_ORE_VEIN_BLOCK_ITEM = ITEMS.register(
+            "deepslate_ancient_ore_vein_block",
+            () -> new Item(
+                    new Item.Properties().useItemDescriptionPrefix().setId(
+                            ResourceKey.create(
+                                    Registries.ITEM, ResourceLocation.parse(
+                                            String.format("%s:%s", ArkFuture.MOD_ID, "deepslate_ancient_ore_vein_block")
+                                    )
+                            )
+                    )
+            )
+    );
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
