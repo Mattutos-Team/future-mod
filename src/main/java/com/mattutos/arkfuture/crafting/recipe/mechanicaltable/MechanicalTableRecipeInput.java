4b59f8e2c0b24c35b4fed12fa7e0d4cdc5e71e79
Brayan <job.brquirino@gmail.com>
2025-04-01 02:19:47 +0100

    feat: adding new customized menu + fixing folders name + fixing recipe match to craft items

--- src/main/java/com/mattutos/arkfuture/crafting/recipe/mechanicaltable/MechanicalTableRecipeInput.java
+++ src/main/java/com/mattutos/arkfuture/crafting/recipe/mechanicaltable/MechanicalTableRecipeInput.java
@@ -1,30 +1,32 @@
-package com.mattutos.arkfuture.crafting.recipe.MechanicalTable;
+package com.mattutos.arkfuture.crafting.recipe.mechanicaltable;
 
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeInput;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class MechanicalTableRecipeInput implements RecipeInput {
-    private static final Logger log = LoggerFactory.getLogger(MechanicalTableRecipeInput.class);
     private final List<ItemStack> inputs;
+    private final ItemStack baseItem;
 
-    public MechanicalTableRecipeInput(List<ItemStack> inputs) {
+    public MechanicalTableRecipeInput(List<ItemStack> inputs, ItemStack baseItem) {
         this.inputs = inputs;
+        this.baseItem = baseItem;
     }
 
     @Override
     public ItemStack getItem(int pIndex) {
+        // Base item is always at the last index
         if (pIndex >= 0 && pIndex < inputs.size()) {
             return inputs.get(pIndex);
+        } else if (pIndex == inputs.size()) {
+            return baseItem;
         }
         return ItemStack.EMPTY;
     }
 
     @Override
     public int size() {
-        return inputs.size();
+        return inputs.size() + 1;  // Account for base item
     }
 }
