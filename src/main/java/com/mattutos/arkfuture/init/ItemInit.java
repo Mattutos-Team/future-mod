3a336dbfa160dc193aa40f087b0f08f8a1feed56
Brayan <job.brquirino@gmail.com>
2025-04-09 20:06:54 +0200

    feat: adding mechanical pliers item with durability logic after craft on mechanical table

--- src/main/java/com/mattutos/arkfuture/init/ItemInit.java
+++ src/main/java/com/mattutos/arkfuture/init/ItemInit.java
@@ -3,6 +3,7 @@ package com.mattutos.arkfuture.init;
 import com.mattutos.arkfuture.ArkFuture;
 import com.mattutos.arkfuture.item.BatteryItem;
 import com.mattutos.arkfuture.item.EnergyPistolItem;
+import com.mattutos.arkfuture.item.MechanicalPliersItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +17,9 @@ public class ItemInit {
     public static final RegistryObject<Item> ANCIENT_ORE_INGOT_ITEM = ITEMS.register("ancient_ore_ingot", () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> BATTERY_10K = ITEMS.register("battery_10k", () -> new BatteryItem(new Item.Properties(), 10_000, 100, 100));
+
+    public static final RegistryObject<Item> MECHANICAL_PLIERS = ITEMS.register("mechanical_pliers", () -> new MechanicalPliersItem(new Item.Properties().durability(50)));
+
 
     public static final RegistryObject<Item> ENERGY_PISTOL = ITEMS.register("energy_pistol", () -> new EnergyPistolItem(new Item.Properties()));
     public static final RegistryObject<Item> ENERGY_PROJECTILE = ITEMS.register("energy_projectile", () -> new Item(new Item.Properties()));
