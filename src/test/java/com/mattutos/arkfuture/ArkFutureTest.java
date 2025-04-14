9708d86d05db52ad9389e352342dc09466309a81
RelryPeSan <relrypesan@gmail.com>
2025-04-14 17:21:56 -0300

    adjusting tests

--- src/test/java/com/mattutos/arkfuture/ArkFutureTest.java
+++ src/test/java/com/mattutos/arkfuture/ArkFutureTest.java
@@ -1,11 +1,22 @@
 package com.mattutos.arkfuture;
 
+import com.mattutos.arkfuture.init.BlockInitTest;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraftforge.event.RegisterGameTestsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = ArkFuture.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ArkFutureTest {
+
     @GameTest
-    public void simpleTest(GameTestHelper helper) {
+    public static void simpleTest(GameTestHelper helper) {
         helper.succeed();
+    }
+
+    @SubscribeEvent
+    public static void onRegisterGameTests(RegisterGameTestsEvent event) {
+        event.register(BlockInitTest.class);
     }
 }
