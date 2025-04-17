9801018c62351d2a1c3d7e286a69db99030b8972
RelryPeSan <relrypesan@gmail.com>
2025-04-17 01:01:52 -0300

    Implemented logic for MechanicalAssemblerBlockEntity
    - Validation of block structure for crafiting
    - Validation of recipes available for entities crafiting
    - Synchronized animation control when crafiting is started

--- src/main/java/com/mattutos/arkfuture/block/entity/AssemblerPartBlockEntity.java
+++ src/main/java/com/mattutos/arkfuture/block/entity/AssemblerPartBlockEntity.java
@@ -2,27 +2,31 @@ package com.mattutos.arkfuture.block.entity;
 
 import com.mattutos.arkfuture.block.entity.util.AFBaseContainerBlockEntity;
 import com.mattutos.arkfuture.init.BlockEntityInit;
-import lombok.extern.slf4j.Slf4j;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.animation.AnimatableManager;
-import software.bernie.geckolib.animation.AnimationController;
-import software.bernie.geckolib.animation.AnimationState;
-import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.animation.*;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-@Slf4j
 public class AssemblerPartBlockEntity extends AFBaseContainerBlockEntity implements GeoBlockEntity {
+    public static final String ENTITY_CORE_KEY = "EntityCorePos";
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     private final ItemStackHandler itemStackHandler;
+
+    private MechanicalAssemblerBlockEntity entityCore;
+    private BlockPos entityCorePos;
 
     public AssemblerPartBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(BlockEntityInit.ASSEMBLER_PART.get(), pPos, pBlockState);
@@ -31,9 +35,15 @@ public class AssemblerPartBlockEntity extends AFBaseContainerBlockEntity impleme
     }
 
     private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> animationState) {
-//        animationState.getController().setAnimation(RawAnimation.begin().then("animation.assembler_part.indicators", Animation.LoopType.LOOP));
+        if (!this.getItem(0).equals(ItemStack.EMPTY) && this.entityCore != null && this.entityCore.isAssembling()) {
+            animationState.getController().setAnimation(
+                    RawAnimation.begin().then("animation.assembler_part.indicators", Animation.LoopType.LOOP)
+            );
 
-        return PlayState.CONTINUE;
+            return PlayState.CONTINUE;
+        }
+
+        return PlayState.STOP;
     }
 
     @Override
@@ -57,12 +67,60 @@ public class AssemblerPartBlockEntity extends AFBaseContainerBlockEntity impleme
     }
 
     @Override
+    protected void loadAdditional(@NotNull CompoundTag pTag, HolderLookup.@NotNull Provider pRegistries) {
+        super.loadAdditional(pTag, pRegistries);
+        this.entityCorePos = null;
+        this.entityCore = null;
+        if (pTag.contains(ENTITY_CORE_KEY)) {
+            CompoundTag tagEntityCorePos = pTag.getCompound(ENTITY_CORE_KEY);
+            this.entityCorePos = new BlockPos(tagEntityCorePos.getInt("x"), tagEntityCorePos.getInt("y"), tagEntityCorePos.getInt("z"));
+            resetEntityCoreByPos();
+        }
+    }
+
+    @Override
+    protected void saveAdditional(@NotNull CompoundTag pTag, HolderLookup.@NotNull Provider pRegistries) {
+        super.saveAdditional(pTag, pRegistries);
+        if (this.entityCore != null) {
+            CompoundTag tagEntityCorePos = new CompoundTag();
+            tagEntityCorePos.putInt("x", this.entityCore.getBlockPos().getX());
+            tagEntityCorePos.putInt("y", this.entityCore.getBlockPos().getY());
+            tagEntityCorePos.putInt("z", this.entityCore.getBlockPos().getZ());
+            pTag.put(ENTITY_CORE_KEY, tagEntityCorePos);
+        }
+    }
+
+    @Override
     public void tickServer() {
+        if (this.level == null) return;
+        resetEntityCoreByPos();
+
+        if (this.entityCore != null) {
+            if (this.level.getBlockEntity(entityCore.getBlockPos()) instanceof MechanicalAssemblerBlockEntity mechanicalAssemblerBlockEntity) {
+                this.entityCore = mechanicalAssemblerBlockEntity;
+            } else {
+                this.entityCore = null;
+            }
+            setChanged();
+        }
     }
 
     @Override
     protected AbstractContainerMenu createMenu(int pContainerId, Inventory pInventory) {
         return null;
+    }
+
+    private void resetEntityCoreByPos() {
+        if (this.entityCorePos == null) return;
+        if (this.level != null && this.level.getBlockEntity(this.entityCorePos) instanceof MechanicalAssemblerBlockEntity mechanicalAssemblerBlockEntity) {
+            this.entityCore = mechanicalAssemblerBlockEntity;
+        }
+        this.entityCorePos = null;
+    }
+
+    public void setEntityCore(MechanicalAssemblerBlockEntity entityCore) {
+        this.entityCore = entityCore;
+        level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), Block.UPDATE_ALL);
     }
 
 }
--- src/main/java/com/mattutos/arkfuture/block/entity/MechanicalAssemblerBlockEntity.java
+++ src/main/java/com/mattutos/arkfuture/block/entity/MechanicalAssemblerBlockEntity.java
@@ -1,27 +1,36 @@
 package com.mattutos.arkfuture.block.entity;
 
+import com.mattutos.arkfuture.block.entity.util.AFBaseContainerBlockEntity;
 import com.mattutos.arkfuture.block.entity.util.AFEnergyContainerBlockEntity;
 import com.mattutos.arkfuture.core.energy.AFEnergyStorage;
+import com.mattutos.arkfuture.crafting.recipe.mechanicalentityassembler.MechanicalEntityAssemblerRecipe;
 import com.mattutos.arkfuture.init.BlockEntityInit;
 import com.mattutos.arkfuture.init.BlockInit;
+import com.mattutos.arkfuture.init.MobAssemblyRecipesInit;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.pattern.BlockInWorld;
 import net.minecraft.world.level.block.state.pattern.BlockPattern;
 import net.minecraft.world.level.block.state.pattern.BlockPatternBuilder;
 import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.*;
-import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.util.GeckoLibUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class MechanicalAssemblerBlockEntity extends AFEnergyContainerBlockEntity implements GeoBlockEntity {
@@ -29,6 +38,11 @@ public class MechanicalAssemblerBlockEntity extends AFEnergyContainerBlockEntity
 
     private final ItemStackHandler itemStackHandler;
     private final AFEnergyStorage energyStorage;
+
+    @Getter
+    private boolean isAssembling = false;
+    private int progress = 0;
+    private int maxProgress = 0;
 
     public MechanicalAssemblerBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(BlockEntityInit.MECHANICAL_ASSEMBLER.get(), pPos, pBlockState);
@@ -38,13 +52,14 @@ public class MechanicalAssemblerBlockEntity extends AFEnergyContainerBlockEntity
     }
 
     private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> animationState) {
-        animationState.getController().setAnimation(RawAnimation.begin().then("animation.mechanical_assembler.powered", Animation.LoopType.LOOP));
+        if (this.isAssembling) {
+            animationState.getController().setAnimation(
+                    RawAnimation.begin().then("animation.mechanical_assembler.powered", Animation.LoopType.LOOP)
+            );
+            return PlayState.CONTINUE;
+        }
 
-        animationState.setData(DataTickets.ACTIVE, energyStorage.getEnergyStored() > 0);
-
-        animationState.setData(DataTickets.ANIM_STATE, 1);
-
-        return PlayState.CONTINUE;
+        return PlayState.STOP;
     }
 
     @Override
@@ -73,8 +88,99 @@ public class MechanicalAssemblerBlockEntity extends AFEnergyContainerBlockEntity
     }
 
     @Override
-    public void tickServer() {
+    protected void loadAdditional(@NotNull CompoundTag pTag, HolderLookup.@NotNull Provider pRegistries) {
+        super.loadAdditional(pTag, pRegistries);
+        this.isAssembling = pTag.getBoolean("isAssembling");
+        this.progress = pTag.getInt("progress");
+        this.maxProgress = pTag.getInt("maxProgress");
+    }
 
+    @Override
+    protected void saveAdditional(@NotNull CompoundTag pTag, HolderLookup.@NotNull Provider pRegistries) {
+        super.saveAdditional(pTag, pRegistries);
+        pTag.putBoolean("isAssembling", this.isAssembling);
+        pTag.putInt("progress", this.progress);
+        pTag.putInt("maxProgress", this.maxProgress);
+    }
+
+    @Override
+    public void tickServer() {
+        if (isAssembling) {
+            BlockPattern pattern = getMechanicalAssemblyPattern();
+            BlockPattern.BlockPatternMatch blockPatternMatch = pattern.find(this.level, this.worldPosition);
+
+            if (blockPatternMatch == null) {
+                resetAssembly();
+                log.warn("Mechanical Assembler Block Entity did not find pattern");
+                return;
+            }
+
+            List<AssemblerPartBlockEntity> partEntities = getPartEntities(blockPatternMatch);
+            List<ItemStack> itemStackList = partEntities.stream()
+                    .map(p -> p.getItem(0))
+                    .toList();
+
+            MechanicalEntityAssemblerRecipe entityAssemblerRecipe = getMatching(itemStackList);
+
+            if (entityAssemblerRecipe == null) {
+                resetAssembly();
+                log.warn("Mechanical Assembler Block Entity did not find recipe");
+                return;
+            }
+
+            if (energyStorage.extractEnergy(entityAssemblerRecipe.energyExpenditurePerTick(), true) != entityAssemblerRecipe.energyExpenditurePerTick()) {
+                log.warn("Mechanical Assembler Block Entity did not have enough energy");
+                return;
+            }
+
+            energyStorage.extractEnergy(entityAssemblerRecipe.energyExpenditurePerTick(), false);
+            this.progress++;
+            log.info("Energy: {}", energyStorage.getEnergyStored());
+
+            if (isFinished()) {
+                resetAssembly();
+                craftEntity(entityAssemblerRecipe, partEntities);
+                setChanged();
+            }
+        }
+    }
+
+    private void resetAssembly() {
+        this.isAssembling = false;
+        this.progress = 0;
+        this.maxProgress = 0;
+    }
+
+    private MechanicalEntityAssemblerRecipe getMatching(List<ItemStack> itemStackList) {
+        return MobAssemblyRecipesInit.findMatching(this.getItem(0), itemStackList);
+    }
+
+    private void craftEntity(MechanicalEntityAssemblerRecipe entityAssemblerRecipe, List<AssemblerPartBlockEntity> partEntities) {
+        Entity entity = entityAssemblerRecipe.entityType().create(this.level);
+
+        if (entity != null) {
+            // Define a posição para spawnar (por exemplo, acima da block entity)
+            BlockPos spawnPos = this.worldPosition.above();
+            entity.moveTo(spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5, 0.0F, 0.0F);
+
+            partEntities.forEach(p -> {
+                if (this.level.getBlockEntity(p.getBlockPos()) instanceof AFBaseContainerBlockEntity assemblerPartBlockEntity) {
+                    assemblerPartBlockEntity.clearContent();
+                }
+            });
+            this.clearContent();
+
+            // Adiciona a entidade no mundo
+            this.level.addFreshEntity(entity);
+        }
+    }
+
+    private boolean isFinished() {
+        if (this.progress >= this.maxProgress) {
+            this.progress = 0;
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -99,18 +205,43 @@ public class MechanicalAssemblerBlockEntity extends AFEnergyContainerBlockEntity
 
         if (blockPatternMatch != null) {
             log.info("Mechanical Assembler Block Entity found pattern");
-            Cow cow = EntityType.COW.create(level);
-            if (cow != null) {
-                // Define a posição para spawnar (por exemplo, acima da block entity)
-                BlockPos spawnPos = worldPosition.above();
-                cow.moveTo(spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5, 0.0F, 0.0F);
 
-                // Adiciona a entidade no mundo
-                level.addFreshEntity(cow);
+            List<AssemblerPartBlockEntity> partEntities = getPartEntities(blockPatternMatch);
+            List<ItemStack> itemStackList = partEntities.stream()
+                    .map(p -> p.getItem(0))
+                    .toList();
+
+            MechanicalEntityAssemblerRecipe entityAssemblerRecipe = getMatching(itemStackList);
+            if (entityAssemblerRecipe != null) {
+                this.isAssembling = true;
+                this.maxProgress = entityAssemblerRecipe.totalTicksPerCraft();
+                this.progress = 0;
+            } else {
+                log.warn("Mechanical Assembler Block Entity did not find recipe");
             }
         } else {
             log.warn("Mechanical Assembler Block Entity did not find pattern");
         }
     }
 
+    private List<AssemblerPartBlockEntity> getPartEntities(BlockPattern.BlockPatternMatch match) {
+        List<AssemblerPartBlockEntity> parts = new ArrayList<>();
+
+        for (int x = 0; x < match.getWidth(); x++) {
+            for (int y = 0; y < match.getHeight(); y++) {
+                for (int z = 0; z < match.getDepth(); z++) {
+                    BlockInWorld blockInWorld = match.getBlock(x, y, z);
+                    if (blockInWorld.getState().getBlock() == BlockInit.ASSEMBLER_PART.get()) {
+                        BlockPos pos = blockInWorld.getPos();
+                        if (blockInWorld.getLevel().getBlockEntity(pos) instanceof AssemblerPartBlockEntity part) {
+                            parts.add(part);
+                            part.setEntityCore(this);
+                        }
+                    }
+                }
+            }
+        }
+
+        return parts;
+    }
 }
